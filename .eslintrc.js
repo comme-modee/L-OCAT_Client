5f6d88b1a83f0fe3fc44eb3ce5da42f152c85bcf
crucial-sub <jssub940@gmail.com>
2024-07-23 21:27:56 +0900

    project init

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,8 @@
 module.exports = {
   root: true,
-  extends: '@react-native',
+  extends: [
+    'eslint:recommended',
+    '@react-native-community',
+    'plugin:prettier/recommended',
+  ],
 };
---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import {Text, View} from 'react-native';
+import 'react-native-gesture-handler';
+
+export default function App() {
+  return (
+    <View>
+      <Text>Hello World</Text>
+    </View>
+  );
+}
