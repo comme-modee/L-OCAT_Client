c1d205ba8fa678c49034bd6dcbb149f7191a7cd7
crucial-sub <jssub940@gmail.com>
2024-07-25 20:00:04 +0900

    feat: react-native-mmkv 관련 함수 작성

---
+++ src/lib/utils/storage.ts
@@ -0,0 +1,34 @@
+import {MMKV} from 'react-native-mmkv';
+
+const storage = new MMKV();
+
+export const setStorage = (key: string, value: any) => {
+  if (typeof value === 'object') {
+    storage.set(key, JSON.stringify(value));
+    return;
+  }
+
+  storage.set(key, value);
+};
+
+export const getStorage = (key: string) => {
+  const value = storage.getString(key);
+
+  if (value === undefined) {
+    return undefined;
+  }
+
+  try {
+    return JSON.parse(value);
+  } catch {
+    return value;
+  }
+};
+
+export const removeStorageItem = (key: string) => {
+  storage.delete(key);
+};
+
+export const clearStorage = () => {
+  storage.clearAll();
+};
